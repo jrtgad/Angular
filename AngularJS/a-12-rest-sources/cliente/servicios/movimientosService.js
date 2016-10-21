48240a8d209fbdd9b6bf4a4809af0fcfa517d111
jrtgad <jruiztoledog@gmail.com>
2016-10-21 13:11:50 +0200

    Cuarto dia

--- AngularJS/a-12-rest-sources/cliente/servicios/movimientosService.js
+++ AngularJS/a-12-rest-sources/cliente/servicios/movimientosService.js
@@ -1,16 +1,16 @@
 (function () {
-	angular.module('servicios').service('movimientosService', movimientosService);
+    angular.module('servicios').service('movimientosService', movimientosService);
 
-	function movimientosService($resource) {
-		/**  Estamos devolviendo recursos, que internamente usan promesas */
+    function movimientosService($resource) {
+        /**  Estamos devolviendo recursos, que internamente usan promesas */
 
-		this.movimientos = $resource(
-			"/api/priv/movimientos/:id", // plantilla de la url del api
-			{ id: '@id' }, // la plantilla se rellena con la propiedad id
-			{ 'update': { method: 'PUT' } }// un método custom con el verobo put para actualizaciones
-		); 
-		
-		this.total = $resource("/api/priv/movimientos/totales/");
+        this.movimientos = $resource(
+            "/api/priv/movimientos/:id", // plantilla de la url del api
+            { id: '@id' }, // la plantilla se rellena con la propiedad id
+            { 'update': { method: 'PUT' } } // un método custom con el verobo put para actualizaciones
+        );
 
-	};
-} ());
+        this.total = $resource("/api/priv/movimientos/totales/");
+
+    };
+}());
